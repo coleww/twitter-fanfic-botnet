a3687be7823bacac61f036492706e54c14f19cec
Cole Willsea <coleww@gmail.com>
2015-08-23 08:56:34 -0700

    fantastic

--- README.md
+++ README.md
@@ -1,13 +1,19 @@
 twitter-fanfic-botnet
 ----------------
 
+tool for making markov bot fan fiction communities on twitter. the bots are seeded with lines from characters from TV shows, and they reply to each other. as well as, you know, anyone else that joins the conversation.
+
+This is intended to be totally opt-in i.e, the bots won't tweet at anyone that doesn't tweet at them. Buuuuuuut it could probably be really easily turned super annoying so I'm intentionally making the documentation kind of shite.
+
 [![NPM](https://nodei.co/npm/twitter-fanfic-botnet.png)](https://nodei.co/npm/twitter-fanfic-botnet/)
 [![js-standard-style](https://img.shields.io/badge/code%20style-standard-brightgreen.svg?style=flat)](https://github.com/feross/standard)
-[![Build Status](https://secure.travis-ci.org/YR_TRAVIS_USER_NAME/twitter-fanfic-botnet.png)](http://travis-ci.org/YR_TRAVIS_USER_NAME/twitter-fanfic-botnet)
 
+npm init in a folder named SOMETHING_bot
 
-// just add stuff to a package.json file!
-{
+```
+  "scripts": {
+    "tweet": "twitterFanficBotnet"
+  },
   twitterFanficBotnet: {
     twitter: {
       consumer_key: 'SPIDERS!!!!',
@@ -18,19 +24,12 @@ twitter-fanfic-botnet
     username: 'mulder_bot',
     charnames: ['fox mulder', 'fox', 'mulder']
   }
-}
-
-
 ```
-- /xFilesBotnet
-  - index.js
-  - /mulder 
-    - package.json
-    - lines.json
-  - /scully
-    - package.json
-    - lines.json
-  - /skinner
-    - package.json
-    - lines.json
-```
+  add some lines for the markov too.
+
+do that a bunch.
+
+write an index.js or something and call it on a cron, from there you can just pick a folder and run the tweet script. 
+
+when a bot awakens, it will maybe tweet an update, and it'll also look at it's recent mentions and maybe reply to them. 
+
--- cmd.js
+++ cmd.js
@@ -1,74 +1,71 @@
 #!/usr/bin/env node
 
-
-var markov = require('markov');
-var m = markov(1);
+var markov = require('markov')
+var m = markov(1)
 var after = require('after')
-var loaded = false
 var fs = require('fs')
 var reqy = require('require-module')
 
 var charMap = {}
 var allTheNames = []
 fs.readdirSync('..').forEach(function (folder) {
-  // if (folder.indexOf('_bot') !== -1) {
-    var deets = reqy('../' + folder + '/package.json' )['twitterFanficBotnet']
-    console.log(deets)
-    console.log(folder)
+  if (folder.indexOf('_bot') !== -1) {
+    var deets = reqy('../' + folder + '/package.json')['twitterFanficBotnet']
     charMap[deets.username] = deets.charnames
     allTheNames = allTheNames.concat(deets.charnames)
-  // }
+  }
 })
 
-console.log('CHARMAP:', charMap)
-console.log('name', allTheNames)
-
-// var Twit = require('twit')
-// var packageJSON = reqy('./package.json')
-// config = packageJSON['twitterFanficBotnet']
-// var T = new Twit(config.twitter)
+var Twit = require('twit')
+var packageJSON = reqy('./package.json')
+var config = packageJSON['twitterFanficBotnet']
+var T = new Twit(config.twitter)
 var lines = reqy('./lines')
 var init = after(lines.length, function () {
   var ogToot = createToot()
   console.log(ogToot)
-  // T.post('statuses/update', {status: ogToot}, function (err, data, response) { // post the next line in reply to the most recent one
-  //   if (err) {
-  //     throw err
-  //   } else {
-  //     console.log(data)
-  //   }
-  // })
-  loaded = true
+  if (Math.random() < 0.33) { // only sometimes
+    T.post('statuses/update', {status: ogToot}, function (err, data, response) { // post the next line in reply to the most recent one
+      if (err) {
+        throw err
+      } else {
+        console.log(data)
+      }
+    })
+  }
+
   console.log('loaded markov')
-  // T.get('statuses/user_timeline', {screen_name: config.username}, function (err, datum, response) {
-  //   if (err) {
-  //     throw err
-  //   } else {
-  //     console.log('got last tweet id')
-  //     T.get('statuses/mentions_timeline', {since_id: datum[0].id_str}, function (err, data, response){
-  //       if (err) {
-  //         throw err
-  //       } else {
-  //         console.log('got recent mentions')
-  //         data.forEach(function (toot) {
-  //           var reply = createReply(toot.text)
-  //           var text = '@' + toot.user.screen_name + ' ' + reply
-  //           var id = toot.id_str
-  //           setTimeout(function () {
-  //             console.log('firing off:', id, text)
-  //             T.post('statuses/update', {status: text, in_reply_to_status_id: id}, function (err, data, response) { // post the next line in reply to the most recent one
-  //               if (err) {
-  //                 throw err
-  //               } else {
-  //                 console.log(data)
-  //               }
-  //             })
-  //           }, 60 * 1000 * ((Math.random() * 5) + 1) * (i + 1))
-  //         })
-  //       }
-  //     })
-  //   }
-  // })
+  T.get('statuses/user_timeline', {screen_name: config.username}, function (err, datum, response) {
+    if (err) {
+      throw err
+    } else {
+      console.log('got last tweet id')
+      T.get('statuses/mentions_timeline', {since_id: datum[0].id_str}, function (err, data, response) {
+        if (err) {
+          throw err
+        } else {
+          console.log('got recent mentions')
+          data.forEach(function (toot, i) {
+            var reply = createReply(toot.text)
+            var text = '@' + toot.user.screen_name + ' ' + reply
+            var id = toot.id_str
+            if (Math.random() < 0.75) {
+              setTimeout(function () {
+                console.log('firing off:', id, text)
+                T.post('statuses/update', {status: text, in_reply_to_status_id: id}, function (err, data, response) { // post the next line in reply to the most recent one
+                  if (err) {
+                    throw err
+                  } else {
+                    console.log(data)
+                  }
+                })
+              }, 60 * 1000 * ((Math.random() * 5) + 1) * (i + 1))
+            }
+          })
+        }
+      })
+    }
+  })
 })
 
 console.log('seeding markov')
@@ -78,11 +75,11 @@ lines.forEach(function (line) {
 
 function createReply (text) {
   // markov funs!
-  return charMapIfy(m.respond(text, 12).join(" "))
+  return charMapIfy(m.respond(text, 12).join(' '))
 }
 
 function createToot () {
-  return charMapIfy(m.fill(m.pick(), 16).join(" "))
+  return charMapIfy(m.fill(m.pick(), 16).join(' '))
 }
 
 function charMapIfy (text) {
